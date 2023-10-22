e5b200675e359e2a2bfc96456ff0fffe514b0b0a
rattevanessa <148609485+rattevanessa@users.noreply.github.com>
2023-10-22 14:40:56 +0200

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,3 +1,30 @@
+trait Pattern {
+    fn draw(&self, c: char);
+}
+
+struct BoxPattern {
+    width: i32,
+    height: i32,
+}
+
+impl BoxPattern {
+    fn new(width: i32, height: i32) -> Self {
+        Self { width, height }
+    }
+}
+
+impl Pattern for BoxPattern {
+    fn draw(&self, c: char) {
+        for _ in 0..self.height {
+            for _ in 0..self.width {
+                print!("{}", c)
+            }
+            println!()
+        }
+    }
+}
+
 fn main() {
-    println!("Hello rpp")
+    let bp = BoxPattern::new(32, 4);
+    bp.draw('#');
 }
